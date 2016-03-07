faf8d2541ccc540135e26fc1e9f0b4dc4631bba0
Bruno Henriques <bruno.s.henriques@tecnico.ulisboa.pt>
2016-03-07 03:48:46 +0000

    Fix negatives

--- scripts.js
+++ scripts.js
@@ -17,51 +17,56 @@ function numChanged(e){
 }
 
 function calc(){
+	var signs = [a,b,c].map(Math.sign);
 	var a = Math.abs(Math.floor(parseInt(na.value)));
 	var b = Math.abs(Math.floor(parseInt(nb.value)));
-	var c = Math.abs(Math.floor(parseInt(nc.value)));
+	var c = Math.floor(parseInt(nc.value));
 	if (isNaN(a+b+c)) return;
-	var ext = [], swap = (a<b);
+	var eea = [], swap = (a<b);
 
 	if (swap){
-		ext[0] = [b, 0, 1, 0];
-		ext[1] = [a, Math.floor(b/a), 0, 1];
+		eea[0] = [b, 0, 1, 0];
+		eea[1] = [a, Math.floor(b/a), 0, 1];
 	} else{
-		ext[0] = [a, 0, 1, 0];
-		ext[1] = [b, Math.floor(a/b), 0, 1];
+		eea[0] = [a, 0, 1, 0];
+		eea[1] = [b, Math.floor(a/b), 0, 1];
 	}
 
 	/* change for to unite with swap  and remove q from here ^ */ 
 
 	for (var i=2; i<1000; i++){
-		ext[i] = [];
-		ext[i][0] = ext[i-2][0] % (ext[i-1][0]*ext[i-1][1]);
-		ext[i][1] = (ext[i][0]!=0) ? Math.floor(ext[i-1][0]/ext[i][0]) : "-";
-		ext[i][2] = ext[i-2][2] - (ext[i-1][2] * ext[i-1][1]);
-		ext[i][3] = ext[i-2][3] - (ext[i-1][3] * ext[i-1][1]);
+		eea[i] = [];
+		eea[i][0] = eea[i-2][0] % (eea[i-1][0]*eea[i-1][1]);
+		eea[i][1] = (eea[i][0]!=0) ? Math.floor(eea[i-1][0]/eea[i][0]) : "-";
+		eea[i][2] = eea[i-2][2] - (eea[i-1][2] * eea[i-1][1]);
+		eea[i][3] = eea[i-2][3] - (eea[i-1][3] * eea[i-1][1]);
 
-		if (ext[i][0] == 0) break;
+		if (eea[i][0] == 0) break;
 	}
-	fillTable(ext);
+	fillTable(eea);
 
 	var x0, y0,
-		xi = ext[i-1][2],
-		yi = ext[i-1][3],
-		gcd = ext[i-1][0];
+		xi = eea[i-1][2],
+		yi = eea[i-1][3],
+		gcd = eea[i-1][0];
 
 	if (c % gcd != 0){
-		ans.innerHTML = "No solution. " + gcd + " is not a divisor of " + c;
+		ans.innerHTML = "No solution. " + gcd + " is not a divisor of " + c+"!";
 		return;
 	}
 
 	if (swap){
-		x0 = (c/gcd)*yi;
-		y0 = (c/gcd)*xi;
-	} else{
-		x0 = (c/gcd)*xi;
-		y0 = (c/gcd)*yi;
+		var tmp = xi;
+		xi = yi;
+		yi = tmp;
 	}
-	ans.innerHTML = "x<sub>0</sub>="+x0+", y<sub>0</sub>="+y0;
+
+	x0 = (c/gcd)*xi;
+	y0 = (c/gcd)*yi;
+
+	ans.innerHTML = a+"("+xi+")"+" + "+b+"("+yi+")"+" = "+gcd;
+	ans.innerHTML += "<br/>"+a+"("+x0+")"+" + "+b+"("+y0+")"+" = "+c/gcd;
+	ans.innerHTML += "<br/>x<sub>0</sub>="+x0+", y<sub>0</sub>="+y0;
 	ans.innerHTML += "<br/>x = "+x0+" + "+(b/gcd)+"t<br/>y = "+y0+" - "+(a/gcd)+"t";
 
 	// do the translation :D
